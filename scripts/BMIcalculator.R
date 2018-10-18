3e47b63c3b7950fa4d6bb8c0949e088b608dbcd3
Tom McAllister <tmcall001@hotmail.com>
2018-10-18 14:34:50 +0100

    created a new R project

---
+++ scripts/BMIcalculator.R
@@ -0,0 +1,29 @@
+#######Function to calculate a BMI
+
+####Input your weight in kg
+
+weight <- 50
+
+###Input your height in m
+
+height <- 1.7
+
+####Calculate Body Mass Index (BMI)
+
+BMI <- weight / (height^2)
+
+###Get your results
+
+if (BMI < 16) {
+  print('Have a couple more chocolate bars')
+} else {
+  if (BMI >= 16 & BMI < 25) {
+    print('Keep on snacking')
+  } else {
+    if (BMI >= 25) {
+      print('Maybe skip that one chocolate bar before sleep')
+    }
+  }
+}
+
+
